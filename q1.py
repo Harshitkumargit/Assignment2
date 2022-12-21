54d9d29cdd8520e757ae0384ecd470f8a72e546b
Harshit Kumar <116452122+Harshitkumargit@users.noreply.github.com>
2022-12-22 01:14:48 +0530

    q1.py

---
+++ q1.py
@@ -0,0 +1,16 @@
+a="Python is a case sensitive language"
+l=len(a)
+print(l) #q1 part a
+
+print(a[::-1]) #q1 part b
+
+b=slice(11,35,1)
+print(a[b])  #q1 part c
+
+c=a.replace("a case sensitive","an object oriented")
+print(c)  #q1 part d
+
+print(a.index("a")) #q1 part e
+
+f=a.replace(" ","")
+print(f)   #q1 part f
