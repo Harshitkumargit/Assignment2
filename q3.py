ad24ddedc147618b1e44d95b58e8c30d3c44b13f
Harshit Kumar <116452122+Harshitkumargit@users.noreply.github.com>
2022-12-22 18:16:48 +0530

    q3.py

---
+++ q3.py
@@ -0,0 +1,7 @@
+a=56
+b=10
+print("a&b = ",a&b)
+print("a|b = ",a|b)
+print("a^b = ",a^b)
+print("bitwise left a = ",a<<2,"  bitwise left b = ",b<<2)
+print("bitwise right a = ",a>>2,"  bitwise right b = ",b>>2)
